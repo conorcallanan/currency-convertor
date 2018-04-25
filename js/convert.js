4fd3303603eb52e054057d8f8f868a137014b5c8
conorcallanan <35769284+conorcallanan@users.noreply.github.com>
2018-04-25 13:36:45 +0100

    added functions for other currencies

--- js/convert.js
+++ js/convert.js
@@ -8,3 +8,35 @@ function euroToPounds(){
   document.getElementById("resultsentence").innerHTML = message;
 
 }
+function euroToDollars(){
+
+  var amount = parseInt(document.getElementById("value1").value);
+//euro to dollars calculation
+  var dollars = amount * 1.06;
+  var message = amount + ' euros converts to ' + dollars + ' US Dollars.';
+  console.log(message);
+  document.getElementById("resultsentence").innerHTML = message;
+
+}
+
+function euroToYen(){
+
+  var amount = parseInt(document.getElementById("value1").value);
+//euro to yen calculation
+  var yen = amount * 121.12;
+  var message = amount + ' euros converts to ' + yen + '  Japanese Yen.';
+  console.log(message);
+  document.getElementById("resultsentence").innerHTML = message;
+
+}
+
+function euroToBitcoin(){
+
+  var amount = parseInt(document.getElementById("value1").value);
+//euro to bitcoin calculation
+  var bitcoin = amount * 0.00082;
+  var message = amount + ' euros converts to ' + bitcoin + ' Bitcoin.';
+  console.log(message);
+  document.getElementById("resultsentence").innerHTML = message;
+
+}
